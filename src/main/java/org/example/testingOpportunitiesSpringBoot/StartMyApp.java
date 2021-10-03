3144695ec08d9e7ec23896935fd290fd025a850a
Alexey <34864818+bet22@users.noreply.github.com>
2021-10-03 23:08:15 +0700

    create app

---
+++ src/main/java/org/example/testingOpportunitiesSpringBoot/StartMyApp.java
@@ -0,0 +1,11 @@
+package org.example.testingOpportunitiesSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartMyApp {
+    public static void main(String[] args) {
+        SpringApplication.run(StartMyApp.class, args);
+    }
+}
