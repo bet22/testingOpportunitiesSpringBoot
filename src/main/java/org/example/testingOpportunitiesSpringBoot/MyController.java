d96fc9659ffc8ae88c5a42fab0bf9c4f9194df7c
bet <bet_222@list.ru>
2021-10-05 19:02:47 +0700

    test annotation swagger

--- src/main/java/org/example/testingOpportunitiesSpringBoot/MyController.java
+++ src/main/java/org/example/testingOpportunitiesSpringBoot/MyController.java
@@ -1,13 +1,18 @@
 package org.example.testingOpportunitiesSpringBoot;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(tags = "Описание контроллера")
 public class MyController {
+
+    @ApiOperation("Описание метода. По умолчанию возвращает имя метода")
     @GetMapping("/")
-    public String getMain() {
-        return "index";
+    public String getMain(@RequestParam() String str) {
+        return str != null ? str : "index";
     }
 }
